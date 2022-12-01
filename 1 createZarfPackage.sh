0e8364101694263469b38d810fa2174aa798717b
juan <jsburckhardt@gmail.com>
2022-12-01 05:10:56 +0000

    chore(demo1): ask if binary was downloaded

--- 1 createZarfPackage.sh
+++ 1 createZarfPackage.sh
@@ -9,7 +9,7 @@ source "$CURR_DIR/helpers/common.sh";
 section "CREATE ZARF PACKAGE"
 # Download and install zarf
 section "Install Zarf"
-info_pause_exec "Download zarf" "wget -O zarf https://zarfbinariessa01.blob.core.windows.net/binaries/bin/zarf?sp=r&st=2022-11-30T10:52:46Z&se=2022-11-30T18:52:46Z&spr=https&sv=2021-06-08&sr=b&sig=Vcf%2BgvKIHv%2B%2FM3pYqyr%2FSkthlickz3eDKY2qr4aPcjM%3D"
+proceed_or_not "Downloaded the binary? Proceed?"
 info_pause_exec "Installing zarf" "chmod a+x zarf && sudo mv zarf /usr/local/bin"
 
 # pause to inspect 
